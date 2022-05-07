1e3f20fae07e8902028657963e9d8fc659df6dad
titutix <antoine.jeanneney@gmail.com>
2022-05-07 18:56:49 +0200

    initial commit :tada:

---
+++ src/main/java/fr/isep/homeexchangemanager/HomeExchangeManagerApplication.java
@@ -0,0 +1,13 @@
+package fr.isep.homeexchangemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeExchangeManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeExchangeManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/isep/homeexchangemanager/HomeExchangeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.isep.homeexchangemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeExchangeManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
