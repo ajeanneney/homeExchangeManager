f792c1699f472f626e290a1bfbde8675a49f3163
ajeanneney <antoine.jeanneney@gmail.com>
2022-05-23 17:25:02 +0200

    enleve la suppression automatique

--- src/main/java/fr/isep/homeexchangemanager/HomeExchangeManagerApplication.java
+++ src/main/java/fr/isep/homeexchangemanager/HomeExchangeManagerApplication.java
@@ -20,11 +20,11 @@ public class HomeExchangeManagerApplication {
         ServiceRepository serviceDao = ctx.getBean(ServiceRepository.class);
         ConstraintRepository constraintDao = ctx.getBean(ConstraintRepository.class);
 
-        //cleaner :
-        reservationDao.deleteAll();
-        photoDao.deleteAll();
-        houseDao.deleteAll();
-        userDao.deleteAll();
+//        //cleaner :
+//        reservationDao.deleteAll();
+//        photoDao.deleteAll();
+//        houseDao.deleteAll();
+//        userDao.deleteAll();
 
         //create constraints
 //        constraintDao.save(new Constraint("Non fumeur", "Cette maison est une maison non fumeur"));
@@ -37,24 +37,24 @@ public class HomeExchangeManagerApplication {
         serviceDao.save(new Service("Nettoyage", "Vous devrez nettoyer la maison à la fin de votre séjour"));
 
 
-        //seeder :
-        User user = new User("admin", "admin", "admin@admin.com", "admin");
-        House house = new House(user, "testHouse", "description of house");
-        House house2 = new House(user, "testHouse2", "description of house number 2");
-        User user2 = new User("user", "user", "user@user.com", "user");
-        House houseUser = new House(user2, "testHouseUser", "description of house User");
-        House houseUser2 = new House(user2, "testHouseUser2", "description of house User number 2");
-        Reservation reservation = new Reservation(houseUser2, user, new Date(), new Date(), false);
-        Reservation reservation1 = new Reservation(houseUser, user, new Date(), new Date(), true);
-
-        userDao.save(user);
-        houseDao.save(house);
-        houseDao.save(house2);
-        userDao.save(user2);
-        houseDao.save(houseUser);
-        houseDao.save(houseUser2);
-        reservationDao.save(reservation);
-        reservationDao.save(reservation1);
+//        //seeder :
+//        User user = new User("admin", "admin", "admin@admin.com", "admin");
+//        House house = new House(user, "testHouse", "description of house");
+//        House house2 = new House(user, "testHouse2", "description of house number 2");
+//        User user2 = new User("user", "user", "user@user.com", "user");
+//        House houseUser = new House(user2, "testHouseUser", "description of house User");
+//        House houseUser2 = new House(user2, "testHouseUser2", "description of house User number 2");
+//        Reservation reservation = new Reservation(houseUser2, user, new Date(), new Date(), false);
+//        Reservation reservation1 = new Reservation(houseUser, user, new Date(), new Date(), true);
+//
+//        userDao.save(user);
+//        houseDao.save(house);
+//        houseDao.save(house2);
+//        userDao.save(user2);
+//        houseDao.save(houseUser);
+//        houseDao.save(houseUser2);
+//        reservationDao.save(reservation);
+//        reservationDao.save(reservation1);
 
     }
 
